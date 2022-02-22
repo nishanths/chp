feb4bcedaf94af477f1f470e251086f225c2f435
Nishanth Shanmugham <nishanth.gerrard@gmail.com>
2022-02-22 22:37:01 +0530

    simplify

--- direction.go
+++ direction.go
@@ -1,25 +1,17 @@
 package chp
 
 // ReceiveOnly provides a way to make a bidrectional channel receive-only from
-// the type system's perspective, without having to write the channel element type
-// explicitly in source code.
-//
-// Note that this function cannot prevent code that already has a reference to c
-// as a bidirectional channel from using it bidirectionally.
-func ReceiveOnly[T any](c chan T) <-chan T {
-	var d <-chan T
-	d = c
-	return d
+// the type system's perspective, without having to specify the channel's
+// element type explicitly in source code.
+func ReceiveOnly[T any](c chan T) (r <-chan T) {
+	r = c
+	return
 }
 
 // SendOnly provides a way to make a bidrectional channel send-only from the
-// type system's perspective, without having to write the channel element type
-// explicitly in source code.
-//
-// Note that this function cannot prevent code that already has a reference to c
-// as a bidirectional channel from using it bidirectionally.
-func SendOnly[T any](c chan T) chan<- T {
-	var d chan<- T
-	d = c
-	return d
+// type system's perspective, without having to specify the channel's element
+// type explicitly in source code.
+func SendOnly[T any](c chan T) (s chan<- T) {
+	s = c
+	return
 }
